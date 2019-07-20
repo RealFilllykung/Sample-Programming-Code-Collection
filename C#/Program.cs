8281f3fda05f82389408e19b5918bf09a4df5d57
RealFilllykung <filllykunggm@gmail.com>
2019-07-21 01:40:07 +0700

    เพิ่มคำสั่งพื้นฐานของภาษา C# รวมไปถึงการเขียนแบบ style OOP

--- C#/Program.cs
+++ C#/Program.cs
@@ -2,6 +2,23 @@
 
 namespace C_
 {
+    class People{
+        private string fname;
+        private string lname;
+        private int age;
+
+        public People(string fn, string ln, int a){
+            fname = fn;
+            lname = ln;
+            age = a;
+        }
+
+        public void showInfo(){
+            Console.WriteLine("Full name: " + fname + " " + lname);
+            Console.WriteLine("age: " + age);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -21,6 +38,64 @@ namespace C_
             Console.WriteLine(myString[0]);//print ค่าตัวแรกของ myString
             Console.WriteLine(myString.IndexOf('e')); //บอกตำแหน่งของตัว e ใน myString
 
+            //ตัวอย่างการรับค่า input
+            Console.Write("Enter the color: ");
+            string myColor = Console.ReadLine();
+            Console.WriteLine("Your input color: " + myColor);
+
+            //ตัวอย่างการสร้าง Arrays
+            int[] myNumArray = {2,3,4,5,6};
+            string[] myNameList = new string[5];
+            myNameList[0]="Fill";
+
+            Console.WriteLine(myNumArray[0]);
+            Console.WriteLine(myNameList[0]);
+
+            //ตัวอย่างการใช้ method ที่สร้างขึ้นมาเอง
+            SayHello();
+            int myExponent = exponentNumber(2,3);
+
+            //if else statement
+            if (myExponent == 8){
+                Console.WriteLine("The number is eight");
+            }
+            else{
+                Console.WriteLine("The number is not eight");
+            }
+
+            //&& || != == คือตัวอย่าง operator
+
+            //ตัวอย่าง loop
+            Boolean loop = true;
+            while(loop){
+                Console.Write("Stop loop? (y|n): ");
+                string ans = Console.ReadLine();
+                if (ans == "n") loop = false;
+            }
+
+            //try catch
+            try{
+                //ทำอะไรสักอย่าง
+                People people1 = new People("Tharathep","Klinla-or",20);
+                people1.showInfo();
+            }
+            catch{
+                //ถ้าเกิดโปรแกรมพังให้ทำอะไร
+                Console.WriteLine("Error has occurred");
+            }
+        }
+
+        //ตัวอย่างการสร้าง method ขึ้นมาเอง
+        static void SayHello(){
+            Console.WriteLine("Hello guys");
+        }
+        static int exponentNumber(int x, int y){
+            int temp = x;
+            for(int i = 0; i < y-1; i++){
+                x *= temp;
+            }
+            Console.WriteLine("Value of x = " + x);
+            return x;
         }
     }
 }
